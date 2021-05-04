69502e2747c2df7addd536baacb8013aca69d7b0
cpelichek <cpelichek@gmail.com>
2021-05-04 12:53:19 -0300

    Add addTask function and AddTask component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import Header from "./components/Header";
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { concatSeries } from "async";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,12 +27,19 @@ function App() {
     },
   ]);
 
+  // Add tasks
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete task by filtering out, setting the tasks to the filtered tasks 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task)=> task.id !== id));
   };
 
-  // Toggle reminder
+  // Toggle reminder, if we where using backend them here we would do fetch requests to our server as well
   const toggleReminder = (id) => {
     setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
   }
@@ -37,6 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : <h3>💪 No more tasks! 🦾</h3>}
     </div>
   );
