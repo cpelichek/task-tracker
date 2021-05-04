5124614d8bcfaa83ca7bbf4a99fdcc4e665a618a
cpelichek <cpelichek@gmail.com>
2021-05-04 10:26:19 -0300

    Pass tasks props down from App - should be improved in the future with Context API for dependency injection or Redux library for state management

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Consulta com o dentista",
+      day: "13 de Maio às 09:00 am",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Entrevista",
+      day: "04 de Maio às 14:45 pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Cozinhar receitas saudáveis",
+      day: "05 de Maio às 11:00 am",
+      reminder: false,
+    },
+  ]);
 
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
