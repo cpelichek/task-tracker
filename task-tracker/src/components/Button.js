82791e7d2443a3c1981dadbb9044d31a9c049985
cpelichek <cpelichek@gmail.com>
2021-05-03 23:55:09 -0300

    Add button onClick function, add button variations on css rules

--- task-tracker/src/components/Button.js
+++ task-tracker/src/components/Button.js
@@ -1,5 +1,21 @@
-const Button = ({ color, text }) => {
-  return <button className={`btn ${color}`}>{text}</button>;
+import PropTypes from "prop-types";
+
+const Button = ({ btnType, text, onClick }) => {
+  return (
+    <button className={`btn ${btnType}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  btnType: "btn-primary",
+};
+
+Button.propTypes = {
+  btnType: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
